22f83b3642084ad8736a36083cd021bd7b217a05
tracer <tracer@tracer.org>
2018-03-31 16:57:06 +0800

    > build sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/libc/stdio/printf.c
+++ src/libc/stdio/printf.c
@@ -5,7 +5,6 @@
 #include <am.h>
 
 /* printf behavior modifier */
-
 #define LEFT_JUSTIFIED   0x1
 #define ALWAYS_SIGNED    0x2
 #define SPACE_PREPENDED  0x4
@@ -35,9 +34,9 @@ static inline int _get_behavior_modifier
 
 static inline int _get_integer(const char *restrict *pfmt) {
   int result = 0;
-  while (isalpha(**pfmt)) {
+  while (isdigit(**pfmt)) {
+    result = result * 10 + **pfmt - '0';
     (*pfmt)++;
-    result = result * 10 + **pfmt;
   }
   return result;
 }
@@ -78,23 +77,25 @@ static inline int _printf_s(const conv_spec *spec, const char* str) {
   return nspace + len;
 }
 
-static inline int _printf_d(const conv_spec *spec, int value) {
+static inline int _printf_d(const conv_spec *spec, int sign,
+    unsigned absv) {
   int precision = spec->precision;
   if (precision == -1) precision = 1; // default precision
   char buf[64];
   int bufc = 0;
-  unsigned absv = value < 0 ? -value : value;
   
   while (absv) {
     buf[bufc++] = '0' + absv % 10;
     absv /= 10;
   }
-  if (value == 0 && precision != 0)
+  if (bufc == 0 && precision != 0)
     buf[bufc++] = '0';
   
   int signlen = 1;
   char signchar;
-  if (value < 0) {
+  if (sign == -1) {
+    signlen = 0;
+  } else if (sign == 1) {
     signchar = '-';
   } else if (spec->behavior & ALWAYS_SIGNED) {
     signchar = '+';
@@ -107,7 +108,7 @@ static inline int _printf_d(const conv_spec *spec, int value) {
   int width = spec->width;
 
   if (spec->behavior & ZERO_PADDING) {
-    if (precision + signlen > width) precision = width;
+    if (precision + signlen < width) precision = width;
     width = 0;
   }
 
@@ -134,15 +135,117 @@ static inline int _printf_d(const conv_spec *spec, int value) {
   return numspace + signlen + numzero + bufc;
 }
 
+static inline int _printf_o(const conv_spec *spec, unsigned value) {
+  int precision = spec->precision;
+  if (precision == -1) precision = 1; // default precision
+  char buf[64];
+  int bufc = 0;
+  
+  while (value) {
+    int cur = value & 0x7;
+    value >>= 3;
+    buf[bufc++] = '0' + cur;
+  }
+
+  if ((bufc == 0 && precision != 0) || 
+      (spec->behavior & ALTERNATIVE_FORM))
+    buf[bufc++] = '0';
+
+  int width = spec->width;
+
+  if (spec->behavior & ZERO_PADDING) {
+    if (precision < width) precision = width;
+    width = 0;
+  }
+
+  int numzero = precision - bufc;
+  if (numzero < 0) numzero = 0;
+
+  int numspace = width - numzero - bufc;
+  if (numspace < 0) numspace = 0;
+
+  if (spec->behavior & LEFT_JUSTIFIED) {
+    _print_nch('0', numzero);
+    for (int i = bufc - 1; i >= 0; i--)
+      _putc(buf[i]);
+    _print_nch(' ', numspace);
+  } else {
+    _print_nch(' ', numspace);
+    _print_nch('0', numzero);
+    for (int i = bufc - 1; i >= 0; i--)
+      _putc(buf[i]);
+  }
+
+  return numspace + numzero + bufc;
+}
+
+static inline int _printf_x(const conv_spec *spec, unsigned value,
+    int capital) {
+  int precision = spec->precision;
+  if (precision == -1) precision = 1; // default precision
+  char buf[64];
+  int bufc = 0;
+  
+  while (value) {
+    int cur = value & 0xf;
+    value >>= 4;
+    if (cur < 10) buf[bufc++] = '0' + cur;
+    else buf[bufc++] = (capital ? 'A' : 'a') + cur - 10;
+  }
+
+  int prefix = 0;  
+  if (bufc == 0) {  // value = 0
+    if (precision != 0)  buf[bufc++] = '0';
+  } else if (spec->behavior & ALTERNATIVE_FORM) {
+      prefix = 2;
+  }
+  
+  int width = spec->width;
+  if (spec->behavior & ZERO_PADDING) {
+    if (precision < width - prefix) precision = width - prefix;
+    width = 0;
+  }
+
+  int numzero = precision - bufc;
+  if (numzero < 0) numzero = 0;
+
+  int numspace = width - numzero - bufc - prefix;
+  if (numspace < 0) numspace = 0;
+
+  if (spec->behavior & LEFT_JUSTIFIED) {
+    if (prefix) {
+      _putc('0');
+      _putc(capital ? 'X' : 'x');
+    }
+    _print_nch('0', numzero);
+    for (int i = bufc - 1; i >= 0; i--)
+      _putc(buf[i]);
+    _print_nch(' ', numspace);
+  } else {
+    _print_nch(' ', numspace);
+    if (prefix) {
+      _putc('0');
+      _putc(capital ? 'X' : 'x');
+    }
+    _print_nch('0', numzero);
+    for (int i = bufc - 1; i >= 0; i--)
+      _putc(buf[i]);
+  }
+
+  return numspace + numzero + bufc + prefix;
+}
+
 int printf(const char *restrict format, ...) {
   va_list args;
   va_start(args, format);
 
   int written = 0;
+
+  int p_int;
+  unsigned p_uint;
   
   conv_spec spec;
   
-#define __printf_putc(ch) written++; _putc(ch)
   do {
     switch (*format) {
       case 0: goto ret;
@@ -183,11 +286,10 @@ int printf(const char *restrict format, ...) {
         } else {
           spec.precision = -1; // default precision
         } 
-        
-        // length modifier is not implemented yet
 
+        // length modifier is not implemented yet
         // conversion format specifier
-        switch (*(format++)) {
+        switch (*format) {
           /* conversion specifier */
           case 'c':
             /* `char' is promoted to `int'. If use char here,
@@ -200,16 +302,39 @@ int printf(const char *restrict format, ...) {
             break;
           case 'd':
           case 'i':
-            written += _printf_d(&spec, va_arg(args, int));
+            p_int = va_arg(args, int);
+            if (p_int < 0) p_uint = -(unsigned)p_int;
+            else p_uint = p_int;
+            written += _printf_d(&spec, p_int < 0, p_uint);
+            break;
+          case 'u':
+            written += _printf_d(&spec, -1, va_arg(args, unsigned));
+            break;
+          case 'o':
+            written += _printf_o(&spec, va_arg(args, unsigned));
+            break;
+          case 'x':
+            written += _printf_x(&spec, va_arg(args, unsigned), 0);
+            break;
+          case 'X':
+            written += _printf_x(&spec, va_arg(args, unsigned), 1);
+            break;
+          case '%':
+            _putc('%');
+            written++;
+            break;
+          case 'n':
+            *va_arg(args, int*) = written;
             break;
           case 0:
             goto ret;
         }
       break;
-      default: __printf_putc(*format);
+      default: 
+        _putc(*format);
+        written++;
     }
   } while (format++);
-#undef __printf_putc
 ret:
   va_end(args);
   return written;
