364ca315ce14443348c854dfc2ba5ccd6f9bc65e
tracer <tracer@tracer.org>
2018-04-01 13:08:53 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/graphics/draw.c
+++ src/graphics/draw.c
@@ -2,7 +2,7 @@
 #include <graphics.h>
 
 void gClear() {
-  memset(_gBuf, 0, 4 * gVideoInfo.width * gVideoInfo.height);
+  memset(_gBuf, 0, 4 * gVideoInfo->width * gVideoInfo->height);
 }
 
 void gSetPixel(int x, int y, gRGB_t color) {
