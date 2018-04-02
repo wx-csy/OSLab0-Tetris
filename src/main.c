e2c8981cdbecbc36ec9b95606cfb9a9d8c2e44aa
tracer <tracer@tracer.org>
2018-04-02 09:16:27 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -24,8 +24,8 @@ int main() {
   while (1) {
     gClear();
     gUpdateKeyboard();
-    if (gKeyPressed(G_KEY_SPACE)) {
-      gDrawRect(0, 0, gVideoInfo.width, gVideoInfo.height, G_BLUE);
+    if (gIsKeyPressed(G_KEY_SPACE)) {
+      gDrawRect(0, 0, gVideoInfo->width, gVideoInfo->height, G_BLUE);
     }
     gRender();
   }
