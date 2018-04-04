68cc3d4bf6f5d611d48c33131612941d50ad0dcf
tracer <tracer@tracer.org>
2018-04-04 11:20:41 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/game/game.c
+++ src/game/game.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <game.h>
 #include <graphics.h>
 
 void screen_update() {
   char buf[32];
   sprintf(buf, "FPS: %d", gGetFPS());
-  gDrawString(20, 20, buf);
+  gDrawString(20, 20, buf, G_WHITE);
 }
 
