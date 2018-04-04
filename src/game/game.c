caedffa52cf1b1870759e98c33505211b4fb4e87
tracer <tracer@tracer.org>
2018-04-04 12:41:02 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/game/game.c
+++ src/game/game.c
@@ -5,6 +5,7 @@
 void screen_update() {
   char buf[32];
   sprintf(buf, "FPS: %d", gGetFPS());
+  gDrawImageA(50, 50, &img_ProjectN_big);
   gDrawString(20, 20, buf, G_WHITE);
 }
 
--- src/graphics/image.c
+++ src/graphics/image.c
@@ -1,6 +1,26 @@
 #include <graphics.h>
 
-int gDrawImageA(int x, int y, const gImage_t *image) {
+static inline uint8_t channel_blend(uint8_t fore, uint8_t back, 
+    uint8_t alpha) {
+  return (fore * alpha + back * (255 - alpha)) / 255;
+}
 
+int gDrawImageA(int x, int y, const gImage_t *image) {
+  uint8_t *data = image->pixel_data;
+  if (image->bytes_per_pixel != 4) return -1;
+  for (int j = y; j < y + image->height; j++) {
+    for  (int i = x; i < x + image->width; i++) {
+      gRBG_t pixel = gGetPixel(i, j);
+      if (pixel == 0xffffffff) continue;
+      uint8_t fr = *(data++), fg = *(data++), 
+              fb = *(data++), fa = *(data++);
+      gSetPixel(i, j, gRGB_t(
+            channel_blend(fr, gRGB_red(pixel), fa);
+            channel_blend(fg, gRGB_green(pixel), fa);
+            channel_blend(fb, gRGB_blue(pixel), fa);
+          ));
+    }
+  }
+  return 0;
 }
 
