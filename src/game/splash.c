640808061f30528e7423d9ffa7a7290d33b0260f
tracer <tracer@tracer.org>
2018-04-05 23:51:51 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/game/splash.c
+++ src/game/splash.c
@@ -25,17 +25,18 @@ void splash_proc() {
     case SPLASH_RUNNING:
       alpha = (uint32_t)(c_now - c_start) * 256;
       alpha = alpha / CLOCKS_PER_SEC / 4;
-      if (alpha > 0xff) status = SPLASH_HOLD;
+      if (alpha > 0xff || gIsKeyPressed(G_KEY_ENTER)
+        status = SPLASH_HOLD;
       gDrawImageAA(120, 20, &img_logo, alpha);
       gDrawImageAA(245, 150, &img_ProjectN, alpha);
-      gDrawStringA(180, 380, "Product of ProjectN Gaming", 
+      gDrawStringA(210, 360, "Product of ProjectN Gaming", 
           G_WHITE, alpha);
       return;
     case SPLASH_HOLD:
       gDrawImageA(120, 20, &img_logo);
       gDrawImageA(245, 150, &img_ProjectN);
-      gDrawString(180, 380, "Product of ProjectN Gaming", G_WHITE);
-      gDrawString(205, 405, "Press 'C' to Start", G_WHITE); 
+      gDrawString(210, 360, "Product of ProjectN Gaming", G_WHITE);
+      gDrawString(245, 385, "Press 'C' to Start", G_WHITE); 
       if (gIsKeyDown(G_KEY_C)) {
         status = SPLASH_END;
         tetris_init();
