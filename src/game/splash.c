ff510eb7f38e2d3d3118781dc780f10bcd60c759
tracer <tracer@tracer.org>
2018-04-06 16:57:23 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/game/splash.c
+++ src/game/splash.c
@@ -16,25 +16,23 @@ static clock_t c_start;
 void splash_proc() {
   clock_t c_now = clock();
   uint32_t alpha;
+  alpha = (uint32_t)(c_now - c_start) * 256;
+  alpha = alpha / CLOCKS_PER_SEC / 3;
+  if (status == SPLASH_RUNNING && 
+       (alpha > 0xff || gIsKeyPressed(G_KEY_RETURN)))
+    status = SPLASH_HOLD;
   switch (status) {
     case SPLASH_NOT_START: 
       c_start = clock();
       status = SPLASH_RUNNING;
       return;
     case SPLASH_RUNNING:
-      alpha = (uint32_t)(c_now - c_start) * 256;
-      alpha = alpha / CLOCKS_PER_SEC / 3;
-      if (alpha > 0xff || gIsKeyPressed(G_KEY_RETURN)) {
-        status = SPLASH_HOLD;
-        goto hold;
-      }
       gDrawImageAA(120, 20, &img_logo, alpha);
       gDrawImageAA(245, 165, &img_ProjectN, alpha);
       gDrawStringA(210, 350, "Product of ProjectN Gaming", 
           G_WHITE, alpha);
       return;
     case SPLASH_HOLD:
-hold:
       gDrawImageA(120, 20, &img_logo);
       gDrawImageA(245, 165, &img_ProjectN);
       gDrawString(210, 350, "Product of ProjectN Gaming", G_WHITE);
