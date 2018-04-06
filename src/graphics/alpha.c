11c5db1f21b4821d960167db4edb1c3226051925
tracer <tracer@tracer.org>
2018-04-06 16:39:40 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/graphics/alpha.c
+++ src/graphics/alpha.c
@@ -6,7 +6,8 @@ void _gInitAlphaTable() {
   for (uint32_t alpha = 0; alpha < 256; alpha++) {
     for (uint32_t fore = 0; fore < 256; fore++) {
       for (uint32_t back = 0; back < 256; back++) {
-        gAlphaTable[alpha][fore][back] = (fore * alpha + back * (0xff - alpha)) / 0xff;
+        _gAlphaTable[alpha][fore][back] = 
+          (fore * alpha + back * (0xff - alpha)) / 0xff;
       }
     }
   }
