3436d50d39057ff45d5347c86b837420aa210dc5
tracer <tracer@tracer.org>
2018-04-06 12:52:26 +0800

    > run sy-chen Linux sy-chen 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/game/gameover.c
+++ src/game/gameover.c
@@ -28,7 +28,7 @@ void gameover_proc() {
       alpha = (uint32_t)(c_now - c_start) * 256;
       alpha = alpha / CLOCKS_PER_SEC / 4;
       if (alpha > 0xff) status = GAMEOVER_HOLD;
-      gDrawImageAA(0, 0, &img_screenshot, 0xff - alpha);
+      gDrawImageA2(0, 0, &img_screenshot, 0xff - alpha);
       gDrawImageAA(80, 171, &img_gameover, alpha);
       return;
     case GAMEOVER_HOLD:
--- src/graphics/image.c
+++ src/graphics/image.c
@@ -19,6 +19,25 @@ int gDrawImageA(int x, int y, const gImage_t *image) {
   return 0;
 }
 
+int gDrawImageA2(int x, int y, const gImage_t *image, uint8_t alpha) {
+  const uint8_t *data = image->pixel_data;
+  if (image->bytes_per_pixel != 4) return -1;
+  for (int j = y; j < y + image->height; j++) {
+    for  (int i = x; i < x + image->width; i++) {
+      gRGB_t pixel = gGetPixel(i, j);
+      if (pixel == 0xffffffff) continue;
+      uint8_t fr = *(data++), fg = *(data++), fb = *(data++);
+      data++;
+      gSetPixel(i, j, gRGB(
+            gChannelBlend(fr, gRGB_red(pixel), alpha),
+            gChannelBlend(fg, gRGB_green(pixel), alpha),
+            gChannelBlend(fb, gRGB_blue(pixel), alpha)
+          ));
+    }
+  }
+  return 0;
+}
+
 int gDrawImageAA(int x, int y, const gImage_t *image, uint8_t alpha) {
   const uint8_t *data = image->pixel_data;
   if (image->bytes_per_pixel != 4) return -1;
